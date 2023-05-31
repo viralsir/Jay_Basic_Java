6d5efbfc36a1cc3fd2e3601d613cb4ec7917020b
ViralSir <viralsir2018@gmail.com>
2023-05-31 11:59:13 +0530

    exception handling is added to the project.

---
+++ src/Exception_handling/trycatchdemo.java
@@ -0,0 +1,56 @@
+package Exception_handling;
+/*
+  Exception Handling : -
+    Exception : IS A MESSAGE WHICH IS BEING DISPLAY WHEN RUN TIME ERROR IS OCCURED.
+
+    try
+    catch
+    finally
+
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class trycatchdemo
+{
+    public static void main(String[] args) {
+
+        boolean flag=true;
+        while(flag==true) {
+
+            try {
+                Scanner s = new Scanner(System.in);
+
+                int no1 = 1, no2 = 1;
+
+                System.out.println("Enter No1:");
+                no1 = s.nextInt();
+                System.out.println("Enter No2:");
+                no2 = s.nextInt();
+
+
+                int ans = no1 / no2;
+
+                System.out.println("Answer :" + ans);
+                flag=false;
+            } catch (ArithmeticException ie) {
+                System.out.println("zero is invalid");
+
+            }
+            //catch (InputMismatchException ie) {
+//                System.out.println("character are invalid");
+//            }
+            finally {
+                if(!flag)
+                    System.out.println("proper");
+                else
+                    System.out.println("improper");
+                System.out.println("finally block is executed.");
+            }
+        }
+        System.out.println("end program");
+
+    }
+
+}
